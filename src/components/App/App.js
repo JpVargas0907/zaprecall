f69ab55abf12aa9ca11154089aab59d705c8704c
João Paulo <joaopaulo0907@gmail.com>
2022-05-14 13:20:27 -0300

    contador do footer finalizado

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import '../../assets/styles/styles.css';
 import '../../assets/styles/reset.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Game from '../Tela Cards/Cards';
+import Game from '../Tela Cards/Game';
 import Dash from '../Tela Inicial /Inicio';
 
 export default function App(){
--- src/components/Tela Cards/Game.js
+++ src/components/Tela Cards/Game.js
@@ -1,11 +1,12 @@
 import logoPequena from '../../assets/_imgs/logo.png'
 import setinha from '../../assets/_imgs/setinha.png'
 import './cards.css'
-import React from 'react'
-import ReactState, {useState}  from 'react'
+import React, { useState } from 'react'
 
 
 export default function Game() {
+    let [contador, setContador] = React.useState(0);
+
     const flashCards = [
         {
             pergunta: 'O que é JSX 18?',
@@ -42,12 +43,8 @@ export default function Game() {
         },
     ];
 
-    const copyFlashCards = [];
-
-    for(let i = 0; i < flashCards.length; i++){
-        copyFlashCards[i] = flashCards[i];
-        embaralhar();
-    }
+    const copyFlashCards = [...flashCards];
+    embaralhar();
 
     function comparador() {
         return Math.random() - 0.5;
@@ -61,10 +58,12 @@ export default function Game() {
     return (
         <div className="content">
             <Logo />
-            {copyFlashCards.map(
-                (card, index) => <Card key = {index} id = {index + 1} pergunta = {card.pergunta} resposta = {card.resposta}/>
-            )}
-            <Footer />
+            <div className='deck-cards'>
+                {copyFlashCards.map(
+                    (card, index) => <Card key={index} id={index + 1} pergunta={card.pergunta} resposta={card.resposta} contador={contador} setContador={setContador} />
+                )}
+            </div>
+            <Footer contador={contador} />
         </div>
     );
 }
@@ -82,34 +81,37 @@ function Card(props) {
     let [classeCard, setClasseCard] = React.useState("card");
     let [classeCardPergunta, setClasseCardPergunta] = React.useState("none");
     let [classeCardResposta, setClasseCardResposta] = React.useState("none");
-    let [ionName, setIonName] = React.useState(<ion-icon onClick={showQuestion} name="play-outline"></ion-icon>);
-    
-    function showQuestion(){
+    let [ionName, setIonName] = React.useState("play-outline");
+
+    function showQuestion() {
         setClasseCard("none");
         setClasseCardPergunta("card-pergunta");
     }
 
-    function showAnswer(){
+    function showAnswer() {
         setClasseCardPergunta("none");
         setClasseCardResposta("card-resposta");
     }
 
-    function userZapAnswer(){
+    function userZapAnswer() {
         setClasseCard("card-zap");
         setClasseCardResposta("none");
-        setIonName(<ion-icon name="checkmark-circle-outline"></ion-icon>)
+        setIonName("checkmark-circle-outline");
+        props.setContador(props.contador + 1);
     }
 
-    function userLateAnswer(){
+    function userLateAnswer() {
         setClasseCard("card-late");
         setClasseCardResposta("none");
-        setIonName(<ion-icon name="help-circle-outline"></ion-icon>)
+        setIonName("help-circle-outline");
+        props.setContador(props.contador + 1);
     }
 
-    function userWrongAnswer(){
+    function userWrongAnswer() {
         setClasseCard("card-wrong");
         setClasseCardResposta("none");
-        setIonName(<ion-icon name="close-circle-outline"></ion-icon>)
+        setIonName("close-circle-outline");
+        props.setContador(props.contador + 1);
     }
 
     return (
@@ -118,7 +120,7 @@ function Card(props) {
                 <p>
                     Pergunta {props.id}
                 </p>
-                {ionName}
+                <ion-icon onClick={showQuestion} name={ionName}></ion-icon>
             </div>
             <div className={classeCardPergunta}>
                 <p className='pergunta'>{props.pergunta}</p>
@@ -142,14 +144,11 @@ function Card(props) {
     );
 }
 
-function Footer() {
+function Footer(props) {
     return (
         <footer>
-            <p>0/4 CONCLUÍDOS</p>
+            <p>{props.contador}/8 CONCLUÍDOS</p>
             <div className='icons'>
-                <ion-icon name="checkmark-circle-outline"></ion-icon>
-                <ion-icon name="help-circle-outline"></ion-icon>
-                <ion-icon name="close-circle-outline"></ion-icon>
             </div>
         </footer>
     );
